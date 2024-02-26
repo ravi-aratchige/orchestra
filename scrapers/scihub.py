b77cca16f2b2b0f638fe3f00797d96113c4309f3
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-02-26 16:30:43 +0530

    REFACTOR: move driver setup to utility script

--- scrapers/scihub.py
+++ scrapers/scihub.py
@@ -1,37 +1,8 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from scrapers.utilities import set_up_driver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-
-
-def set_up_driver():
-    """Set up a Chrome webdriver to browse the web.
-
-    Returns:
-        WebDriver: the Chrome webdriver
-    """
-    # ensure browser does not close after end of script execution
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-
-    # set download path for PDFs
-    download_dir = "/home/ravindu-aratchige/Downloads"
-    chrome_options.add_experimental_option(
-        "prefs",
-        {
-            "download.default_directory": download_dir,
-            "download.prompt_for_download": False,
-            "download.directory_upgrade": True,
-            "plugins.always_open_pdf_externally": True,
-        },
-    )
-
-    # set up the Chromium web driver
-    driver = webdriver.Chrome(options=chrome_options)
-
-    return driver
 
 
 def get_paper_metadata(driver, search_str):
