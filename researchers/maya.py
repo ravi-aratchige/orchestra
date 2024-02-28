fc3ba41411918c0a6a1e2247e028208f7c8e060e
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-02-28 23:35:01 +0530

    REFACTOR: Maya's abilities separated into functions

--- researchers/maya.py
+++ researchers/maya.py
@@ -1,62 +1,82 @@
+"""Maya performs inference based on the user's query and context retrieved by Stella.
+"""
+
 import os
 import sys
-from dotenv import load_dotenv
-from langchain_community.llms import Cohere
 from langchain.prompts import PromptTemplate
+from .utilities import configure_llm
 
 # load scrapers
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from scrapers.utilities import set_up_driver
 from scrapers.pubmed import get_metadata_and_abstract
 
-# load Cohere API key
-load_dotenv()
-os.environ["COHERE_API_KEY"] = os.getenv("COHERE_API_KEY")
 
-# configure LLM
-llm = Cohere(model="command-xlarge-nightly", temperature=0.5, max_tokens=800, k=0, p=1)
+def get_context(search_phrase, article_count):
+    driver = set_up_driver()
+    context = ""
 
-# setup prompt template
-prompt_template = PromptTemplate.from_template(
-    """You are Maya, a friendly and helpful AI chatbot who is proficient
-in answering queries about medical research. You have been asked the following question:
+    for i in range(1, article_count):
+        search_result = get_metadata_and_abstract(driver, search_phrase)
+        pass
 
-{question}
 
-You must answer based on the following context:
+def initialize_prompt(question):
+    # setup prompt template
+    prompt_template = PromptTemplate.from_template(
+        """You are Maya, a friendly and helpful AI chatbot who is proficient
+    in answering queries about medical research. You have been asked the following question:
 
-{context}
+    {question}
 
----
+    You must answer based on the following context:
 
-Do not attempt to answer from your own knowledge. Provide citations in your answer using the information
-provided in the above context.
+    {context}
 
-Your response:
-"""
-)
+    ---
 
-# initialize inputs
-question = (
-    "What is the name of the study done regarding transmucosal implant placement?"
-)
-search_phrase = "transmucosal implant placement"
-driver = set_up_driver()
-search_result = get_metadata_and_abstract(driver, search_phrase)
-context = f"""Title: {search_result["title"]}
-PMID: {search_result['pmid']}
-Authors: {search_result['authors']}
-Abstract: {search_result['abstract']}
-"""
+    Do not attempt to answer from your own knowledge. Provide citations in your answer using the information
+    provided in the above context.
 
-# initialize prompt with inputs
-prompt = prompt_template.format(
-    question=question,
-    context=context,
-)
+    Your response:
+    """
+    )
 
-print(prompt)
+    # initialize inputs
+    search_phrase = "transmucosal implant placement"
+    driver = set_up_driver()
+    search_result = get_metadata_and_abstract(driver, search_phrase)
+    context = f"""Title: {search_result["title"]}
+    PMID: {search_result['pmid']}
+    Authors: {search_result['authors']}
+    Abstract: {search_result['abstract']}
+    """
 
-# get output from model
-output = llm(prompt)
-print(output)
+    # initialize prompt with inputs
+    prompt = prompt_template.format(
+        question=question,
+        context=context,
+    )
+
+    print(prompt)
+    return prompt
+
+
+def main():
+    # set up LLM
+    llm = configure_llm()
+
+    # initialize prompt
+    prompt = initialize_prompt(
+        question=(
+            "What is the name of the study done regarding transmucosal implant placement?"
+        )
+    )
+
+    # get output from model
+    output = llm(prompt)
+    print(f"Output: {output}")
+
+
+if __name__ == "__main__":
+    main()
