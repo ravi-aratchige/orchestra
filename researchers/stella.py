99acfdb9f9744d9c988a149120bcf6d903a4fde6
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-03-01 13:54:05 +0530

    FEAT: Stella can now intelligently calculate the number of articles needed

--- researchers/stella.py
+++ researchers/stella.py
@@ -1,8 +1,15 @@
 """Stella retrieves context for Maya to perform inference.
+
+Additionally, Stella can intelligently determine the number of articles to be retrieved for Maya,
+based on the user's query.
+
+Stella is part of the `researchers` package.
 """
 
 import os
 import sys
+from webbrowser import get
+from langchain.prompts import PromptTemplate
 
 # add parent directory to Python path
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,6 +18,12 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 # load scrapers
 from scrapers.utilities import set_up_driver
 from scrapers.pubmed import get_metadata_and_abstract
+
+# load researcher utilities
+from researchers.utilities import configure_llm
+
+# load Orion's functions
+from researchers.orion import get_search_phrases
 
 
 def get_context(search_phrase, article_count=1):
@@ -47,10 +60,93 @@ def get_context(search_phrase, article_count=1):
     return context
 
 
+def initialize_prompt_for_article_count_calculation(query):
+    # set up prompt template
+    prompt_template = PromptTemplate.from_template(
+        """You are Stella, a friendly and helpful AI chatbot in a team of AI chatbots who are assisting a user in medical research.
+
+When a user is conducting medical research with your help, literature must be retrieved from open-access sources to support
+the responses provided by the team of AI chatbot to the user's queries. The literature thus retrieved will be provided to Maya,
+another chatbot in your team, to perform inference and provide a meaningful response to the user's query.
+
+Your role is to decide how many articles must be retrieved based on the user's query.
+
+If the user has specified the number of articles required in their query itself, you must generate that number as an integer.
+
+Note: do not generate the number as a word. For instance, 3 would a valid output, but "three" would not.
+
+An example of this is given below:
+
+Query: Give me five research papers about malignant brain tumors
+Response: 5
+
+Another similar example:
+
+Query: I want 12 articles written by Gopal et. al.
+Response: 12
+
+If the user's query hints at one research paper, generate 1 as the output.
+
+For example:
+
+Query: Tell me about a research paper written about genomic characterization
+Response: 1
+
+For scenarios where the user's query doesn't specify any number, and Maya could benefit from having multiple research papers
+to validate her answers, generate 3 as the output.
+
+For example:
+
+Query: What can you tell me about pancreas anatomy?
+Response: 3
+
+Only generate the integer value as the output. Do not say anything else. Now analyze this query and decide the number of research papers necessary:
+
+Query: {query}
+Response:"""
+    )
+
+    # initialize prompt with user's query
+    prompt = prompt_template.format(
+        query=(query),
+    )
+
+    return prompt
+
+
+def calculate_article_count(query):
+    # set up LLM
+    llm = configure_llm()
+
+    # calculate number of articles required
+    prompt = initialize_prompt_for_article_count_calculation(query)
+    print(prompt)
+
+    # determine number of articles required
+    try:
+        article_count = int(llm.invoke(prompt))
+    except ValueError:
+        print(
+            f"ERROR: Stella did not generate a valid integer! Defaulting to 3 articles..."
+        )
+        article_count = 3
+
+    print(
+        f"Stella has determined that {article_count} articles are sufficient for this."
+    )
+
+    return article_count
+
+
 def main():
-    # configure inputs for context retrieval
-    search_phrase = "transmucosal implant placement"
-    article_count = 3
+    # initialize demo user query
+    query = (
+        "I want to know about research done regarding transmucosal implant placement"
+    )
+
+    # get search phrases and article count for demo query
+    search_phrase = get_search_phrases(query)
+    article_count = calculate_article_count(query)
 
     # retrieve context for relevant search
     context = get_context(search_phrase, article_count)
